58b15ee23fc9930e3bab642c46653e55b2033821
rajat2310282 <rajat2310282@gmail.com>
2025-07-07 21:51:56 +0530

    project done

--- event-management/backend/server.js
+++ event-management/backend/server.js
@@ -12,10 +12,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log("MongoDB connected successfully"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
